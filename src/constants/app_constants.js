cefb6dd3e0ba91a22abaffa4402a3b6da3936e60
Mohit Chouhan <Mohit.Chouhan.dev@gmail.com>
2025-05-29 10:10:21 +0530

    No enum in database

---
+++ src/constants/app_constants.js
@@ -0,0 +1,9 @@
+export class AppConstants{
+static userDefaltRole=0;
+}
+
+export const UserRole={
+ user:0,
+ admin:1,
+ organizer:2
+}
--- src/response/user.js
+++ src/response/user.js
@@ -2,10 +2,11 @@
 
 export class UserResponse{
     constructor(data){
-        this.userName=data.userName?data.userName:"";
-        this.email=data.email?data.email:"";
-        this.profileImage=data.profileImage?data.profileImage:"";
-        this.profileImage=data.profileImage?data.profileImage:"";
-        this.userRole=data.userRole?data.userRole:"";
+        this.id=data._id??"";
+        this.userName=data.userName??"";
+        this.email=data.email??"";
+        this.profileImage=data.profileImage??"";
+        this.profileImage=data.profileImage??"";
+        this.userRole=data.userRole??"";
     }
 }
